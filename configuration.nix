aad71cef06315e0c0f0157575d99d40d8d04b388
sandptel <sandeeppatel626735@gmail.com>
2024-06-11 18:59:42 +0530

    .

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,9 @@ programs.sway.enable=true;
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
+
   services.xserver.displayManager.gdm.enable = true;
+# services.xserver.displayManager.lightdm.enable=true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
